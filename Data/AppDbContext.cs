03a9aa4663c08160e18f2d3a1a0bbd8e06d65385
Matis <matis@matis.io>
2025-11-17 20:06:46 -0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2DDA.Models;
+
+namespace Parcial2DDA.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ejemplo> Ejemplos { get; set; }
+    }
+}
---
+++ Models/Ejemplo.cs
@@ -0,0 +1,8 @@
+﻿namespace Parcial2DDA.Models
+{
+    public class Ejemplo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
